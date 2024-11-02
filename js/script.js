e36dbf8b1391bbf95fc4816f735aead19ab4332b
yuriiSw <yuriwsouza@outlook.com>
2024-11-02 18:25:05 -0300

    att_02

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 const reponsivenav = document.querySelector(".reponsivenav");
 const nav = document.querySelector(".nav");
-
-// Alterna a classe 'active' na navegação quando o botão de menu é clicado
 reponsivenav.addEventListener("click", () => nav.classList.toggle("active"));
 
 document.addEventListener('DOMContentLoaded', function() {
     const startButton = document.querySelector('.cta-button');
 
-    // Adiciona efeitos de sombra ao passar o mouse sobre o botão
     if (startButton) {
         startButton.addEventListener('mouseover', function() {
             startButton.style.boxShadow = '0 4px 15px rgba(0, 0, 0, 0.3)';
@@ -17,4 +14,11 @@ document.addEventListener('DOMContentLoaded', function() {
             startButton.style.boxShadow = 'none';
         });
     }
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) { 
+            nav.classList.add('scrolled');
+        } else {
+            nav.classList.remove('scrolled');
+        }
+    });
 });
